207d39d319f9c8ed20b8d871e9873218f2a4b5ba
Ivan Kushchev <ibortsov14@gmail.com>
2021-06-16 19:45:01 +0300

    Fix MultiHash function

--- 2/99_hw/signer/signer.go
+++ 2/99_hw/signer/signer.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"hash/crc32"
 	"sort"
 	"strconv"
 	"strings"
 	"sync"
-	"sync/atomic"
-	"time"
 )
 
 func ExecutePipeline(jobs ...job) {
@@ -28,9 +24,14 @@ func ExecutePipeline(jobs ...job) {
 			defer close(out)
 		}(channelsPool[i], channelsPool[i+1], jobFunc, wg)
 	}
+
 	wg.Wait()
 }
 
+/*
+ Calculate crc32() hash and crc32(md5()) from input data
+ and pass it further to MultiHash
+*/
 func SingleHash(in, out chan interface{}) {
 	wg := &sync.WaitGroup{}
 
@@ -82,8 +83,11 @@ func calcSingleHash(data, md5Hash string, out chan interface{}) {
 	wg.Wait()
 }
 
+/*
+ Calculate MultiHash and pass it further to CombineResults
+*/
 func MultiHash(in, out chan interface{}) {
-	wgForClosingChannel := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 
 	for rawData := range in {
 		singleHashResult, ok := rawData.(string)
@@ -91,43 +95,42 @@ func MultiHash(in, out chan interface{}) {
 			fmt.Print("cant convert data to string in MultiHash function")
 		}
 
-		type thStruct struct {
-			index int
-			hash  string
-		}
+		wg.Add(1)
 
-		channel := make(chan thStruct)
-		wgForClosingChannel.Add(6)
-		for i := 0; i < 6; i++ {
-			th := strconv.Itoa(i)
-			go func(data string, channel chan thStruct, wgForClosingChannel *sync.WaitGroup, index int) {
-				result := DataSignerCrc32(data)
-				resultStruct := thStruct{
-					index: index,
-					hash:  result,
-				}
-				channel <- resultStruct
-				defer wgForClosingChannel.Done()
-			}(th+singleHashResult, channel, wgForClosingChannel, i)
-		}
-		go func(wgForClosingChannel *sync.WaitGroup, channel chan thStruct) {
-			wgForClosingChannel.Wait()
-			close(channel)
-		}(wgForClosingChannel, channel)
+		go func(wg *sync.WaitGroup, data string, out chan interface{}) {
+			calcMultiHash(data, out)
+			defer wg.Done()
+		}(wg, singleHashResult, out)
 
-		resultMap := make(map[int]string)
-		for thHash := range channel {
-			resultMap[thHash.index] = thHash.hash
-		}
-
-		result := ""
-		for i := 0; i < 6; i++ {
-			result += resultMap[i]
-		}
-		out <- result
 	}
+	wg.Wait()
 }
 
+/*
+ Calculate six crc32 hashes (index + value from SingleHash) and concatenate them
+*/
+func calcMultiHash(data string, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+
+	hashes := make([]string, 6)
+	wg.Add(6)
+	for i := 0; i < 6; i++ {
+		th := strconv.Itoa(i)
+		go func(data string, wgForClosingChannel *sync.WaitGroup, index int, hashes []string) {
+			hash := DataSignerCrc32(data)
+			hashes[index] = hash
+			defer wgForClosingChannel.Done()
+		}(th+data, wg, i, hashes)
+	}
+	wg.Wait()
+
+	result := strings.Join(hashes, "")
+	out <- result
+}
+
+/*
+ Sort all of the results and concatenate them with "_" sign
+*/
 func CombineResults(in, out chan interface{}) {
 	combinedResult := make([]string, 0)
 
@@ -143,104 +146,4 @@ func CombineResults(in, out chan interface{}) {
 	result := strings.Join(combinedResult, "_")
 
 	out <- result
-}
-
-func main() {
-	testExpected := "1173136728138862632818075107442090076184424490584241521304_1696913515191343735512658979631549563179965036907783101867_27225454331033649287118297354036464389062965355426795162684_29568666068035183841425683795340791879727309630931025356555_3994492081516972096677631278379039212655368881548151736_4958044192186797981418233587017209679042592862002427381542_4958044192186797981418233587017209679042592862002427381542"
-	testResult := "NOT_SET"
-
-	var (
-		DataSignerSalt         string = "" // на сервере будет другое значение
-		OverheatLockCounter    uint32
-		OverheatUnlockCounter  uint32
-		DataSignerMd5Counter   uint32
-		DataSignerCrc32Counter uint32
-	)
-	OverheatLock = func() {
-		atomic.AddUint32(&OverheatLockCounter, 1)
-		for {
-			if swapped := atomic.CompareAndSwapUint32(&dataSignerOverheat, 0, 1); !swapped {
-				fmt.Println("OverheatLock happend")
-				time.Sleep(time.Second)
-			} else {
-				break
-			}
-		}
-	}
-	OverheatUnlock = func() {
-		atomic.AddUint32(&OverheatUnlockCounter, 1)
-		for {
-			if swapped := atomic.CompareAndSwapUint32(&dataSignerOverheat, 1, 0); !swapped {
-				fmt.Println("OverheatUnlock happend")
-				time.Sleep(time.Second)
-			} else {
-				break
-			}
-		}
-	}
-	DataSignerMd5 = func(data string) string {
-		atomic.AddUint32(&DataSignerMd5Counter, 1)
-		OverheatLock()
-		defer OverheatUnlock()
-		data += DataSignerSalt
-		dataHash := fmt.Sprintf("%x", md5.Sum([]byte(data)))
-		time.Sleep(10 * time.Millisecond)
-		return dataHash
-	}
-	DataSignerCrc32 = func(data string) string {
-		atomic.AddUint32(&DataSignerCrc32Counter, 1)
-		data += DataSignerSalt
-		crcH := crc32.ChecksumIEEE([]byte(data))
-		dataHash := strconv.FormatUint(uint64(crcH), 10)
-		time.Sleep(time.Second)
-		return dataHash
-	}
-
-	inputData := []int{0, 1, 1, 2, 3, 5, 8}
-	//inputData := []int{0, 1}
-
-	hashSignJobs := []job{
-		job(func(in, out chan interface{}) {
-			for _, fibNum := range inputData {
-				out <- fibNum
-			}
-		}),
-		job(SingleHash),
-		job(MultiHash),
-		job(CombineResults),
-		job(func(in, out chan interface{}) {
-			dataRaw := <-in
-			data, ok := dataRaw.(string)
-			if !ok {
-				fmt.Println("cant convert result data to string")
-			}
-			testResult = data
-		}),
-	}
-
-	start := time.Now()
-
-	ExecutePipeline(hashSignJobs...)
-
-	end := time.Since(start)
-
-	expectedTime := 3 * time.Second
-
-	if testExpected != testResult {
-		fmt.Printf("results not match\nGot: %v\nExpected: %v", testResult, testExpected)
-		fmt.Println()
-	}
-
-	if end > expectedTime {
-		fmt.Printf("execition too long\nGot: %s\nExpected: <%s", end, time.Second*3)
-		fmt.Println()
-	}
-
-	// 8 потому что 2 в SingleHash и 6 в MultiHash
-	if int(OverheatLockCounter) != len(inputData) ||
-		int(OverheatUnlockCounter) != len(inputData) ||
-		int(DataSignerMd5Counter) != len(inputData) ||
-		int(DataSignerCrc32Counter) != len(inputData)*8 {
-		fmt.Println("not enough hash-func calls")
-	}
 }
