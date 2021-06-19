d37c00a56ca0f50119587a7887fc104467080c57
Ivan Kushchev <ibortsov14@gmail.com>
2021-06-19 18:59:18 +0300

    Get rid of regexp and unnecessary loops for browsers

--- 3/99_hw/fast.go
+++ 3/99_hw/fast.go
@@ -1,18 +1,87 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	/*
-		!!! !!! !!!
-		обратите внимание - в задании обязательно нужен отчет
-		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
-		так же обратите внимание на команду в параметром -http
-		перечитайте еще раз задание
-		!!! !!! !!!
-	*/
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	var seenBrowsers []string
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]map[string]interface{}, 0)
+	for _, line := range lines {
+		user := make(map[string]interface{}, 10)
+		// fmt.Printf("%v %v\n", err, line)
+		err := json.Unmarshal([]byte(line), &user)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers, ok := user["browsers"].([]interface{})
+		if !ok {
+			continue
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				continue
+			}
+			if ok := strings.Contains(browser, "Android"); ok {
+				isAndroid = true
+			} else if ok = strings.Contains(browser, "MSIE"); ok {
+				isMSIE = true
+			} else {
+				continue
+			}
+
+			notSeenBefore := true
+			for _, item := range seenBrowsers {
+				if item == browser {
+					notSeenBefore = false
+				}
+			}
+			if notSeenBefore {
+				seenBrowsers = append(seenBrowsers, browser)
+				uniqueBrowsers++
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := strings.Replace(user["email"].(string), "@", " [at] ", 1)
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
