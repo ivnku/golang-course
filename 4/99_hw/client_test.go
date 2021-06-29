8561c8b4a50b64c401f0d179e98081fc4f8a77ee
Ivan Kushchev <ibortsov14@gmail.com>
2021-06-30 00:47:10 +0300

    Add tests

--- 4/99_hw/client_test.go
+++ 4/99_hw/client_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -49,7 +48,8 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 	searchRequest, err := prepareParameters(*r)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte(err.Error()))
+		errorJson, _ := json.Marshal(&SearchErrorResponse{err.Error()})
+		_, _ = w.Write(errorJson)
 		return
 	}
 
@@ -122,7 +122,7 @@ func prepareParameters(r http.Request) (SearchRequest, error) {
 	}
 	if orderField := r.URL.Query().Get("order_field"); orderField != "" {
 		if orderField != "Id" && orderField != "Age" && orderField != "Name" {
-			return searchRequest, &MyError{errorMessage: "ErrorBadOrderField"}
+			return searchRequest, &MyError{"ErrorBadOrderField"}
 		}
 		searchRequest.OrderField = orderField
 	}
@@ -175,38 +175,193 @@ func order(field string, direction int, users []UserXML) {
 
 // TESTS
 
-func TestTmp(t *testing.T) {
-	searchServer := httptest.NewServer(http.HandlerFunc(SearchServer))
-	defer searchServer.Close()
+func initFixtures(token string, handler func(w http.ResponseWriter, r *http.Request)) (*httptest.Server, *SearchClient, SearchRequest) {
+	if handler == nil {
+		handler = SearchServer
+	}
+	searchServer := httptest.NewServer(http.HandlerFunc(handler))
 
-	searchClient := &SearchClient{"servertoken", searchServer.URL}
 	searchRequest := SearchRequest{
-		Limit:      3,
-		Offset:     1,
-		Query:      "Guerr",
-		OrderField: "Age",
-		OrderBy:    -1,
+		Limit:      10,
+		Offset:     0,
+		Query:      "",
+		OrderField: "",
+		OrderBy:    0,
 	}
 
-	res, _ := searchClient.FindUsers(searchRequest)
+	return searchServer, &SearchClient{token, searchServer.URL}, searchRequest
+}
 
-	fmt.Printf("response is %v", *res)
+func TestFindUsersByLastName(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, nil)
+	defer searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestNegativeLimit(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, nil)
+	defer searchServer.Close()
+
+	searchRequest.Limit = -1
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "limit must be > 0" {
+		t.Error(err)
+	}
+}
+
+func TestLimitMoreThanTwentyFive(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, nil)
+	defer searchServer.Close()
+
+	searchRequest.Limit = 30
+
+	res, err := searchClient.FindUsers(searchRequest)
+
+	if err != nil || len(res.Users) != 25 {
+		t.Error(err)
+	}
+}
+
+func TestNegativeOffset(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, nil)
+	defer searchServer.Close()
+
+	searchRequest.Offset = -1
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "offset must be > 0" {
+		t.Error(err)
+	}
+}
+
+func TestSearchServerFatalError(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	})
+	defer searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "SearchServer fatal error" {
+		t.Error(err)
+	}
+}
+
+func TestBadOrderField(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, nil)
+	defer searchServer.Close()
+
+	searchRequest.OrderField = "InvalidOrderField"
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "OrderFeld InvalidOrderField invalid" {
+		t.Error(err)
+	}
+}
+
+func TestCantUnpackErrorJson(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("some wrong structured error message json"))
+		return
+	})
+	defer searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "cant unpack error json: invalid character 's' looking for beginning of value" {
+		t.Error(err)
+	}
+}
+
+func TestUnknownBadRequestError(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+		errorJson, _ := json.Marshal(&SearchErrorResponse{"SomeUnknownErrorMessage"})
+		_, _ = w.Write(errorJson)
+		return
+	})
+	defer searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "unknown bad request error: SomeUnknownErrorMessage" {
+		t.Error(err)
+	}
+}
+
+func TestCantUnpackResultJson(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, func(w http.ResponseWriter, r *http.Request) {
+		resultJson, _ := json.Marshal("resultInvalidJson")
+		_, _ = w.Write(resultJson)
+		return
+	})
+	defer searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || !strings.Contains(err.Error(), "cant unpack result json") {
+		t.Error(err)
+	}
+}
+
+func TestDataLengthNotEqualLimit(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures(token, func(w http.ResponseWriter, r *http.Request) {
+		user := []User{{14, "UserName", 23, "about info", "male"}}
+		resultJson, _ := json.Marshal(user)
+		_, _ = w.Write(resultJson)
+		return
+	})
+	defer searchServer.Close()
+
+	res, err := searchClient.FindUsers(searchRequest)
+
+	if err != nil || len(res.Users) != 1 {
+		t.Error(err)
+	}
+}
+
+func TestTimeoutError(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures("InvalidToken", nil)
+	searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || !strings.Contains(err.Error(), "timeout for") {
+		t.Error(err)
+	}
+}
+
+func TestUnknownHttpRequestError(t *testing.T) {
+	searchServer, searchClient, searchRequest := initFixtures("InvalidToken", nil)
+	searchServer.Close()
+
+	searchClient.URL = ""
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || !strings.Contains(err.Error(), "unknown error") {
+		t.Error(err)
+	}
 }
 
 func TestInvalidToken(t *testing.T) {
-	//searchServer := httptest.NewServer(http.HandlerFunc(SearchServer))
-	//defer searchServer.Close()
-	//
-	//searchClient := &SearchClient{"InvalidToken", searchServer.URL}
-	//searchRequest := SearchRequest{}
-	//
-	//_, err := searchClient.FindUsers(searchRequest)
-	//
-	//if err == nil {
-	//	t.Error("No error for invalid token")
-	//}
-	//
-	//if err.Error() != "Bad AccessToken" {
-	//	t.Error("Invalid error message")
-	//}
+	searchServer, searchClient, searchRequest := initFixtures("InvalidToken", nil)
+	defer searchServer.Close()
+
+	_, err := searchClient.FindUsers(searchRequest)
+
+	if err == nil || err.Error() != "Bad AccessToken" {
+		t.Error(err)
+	}
 }
